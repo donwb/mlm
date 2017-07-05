fb334fa6ae56fd8662a99cc9d134adda49e5dbaa
Don Browning <don.browning@gmail.com>
2017-07-05 00:15:39 -0400

    some pandas

--- lesson3.py
+++ lesson3.py
@@ -4,3 +4,9 @@ names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 data = read_csv(url, names=names)
 print(data.shape)
 
+print(data.head(n=5))
+print(data.tail(3))
+
+print(data.columns)
+
+print(data.describe())
